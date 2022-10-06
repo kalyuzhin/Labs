54a96373394474b02b67f35b214f7d960ada182f
Марк Калюжин <m.kalyuzhin@gmail.com>
2022-10-06 14:53:00 +0300

    Вывод типа года

--- DateTime.pas
+++ DateTime.pas
@@ -1,3 +1,8 @@
 ﻿begin
-  
+  var days := 365;
+  Print('Hello github');
+  if days mod 365 = 1 then
+    Println('Високосный год')
+  else if days mod 365 = 0 then
+    Println('Не високосный год');
 end.
